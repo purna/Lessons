f317b51d2bd1288569ab9aaa95f6aec2596574da
Nigel Morris <nigel@pixelagent.co.uk>
2023-09-27 14:34:27 +0100

    update 27/09/23

--- Assets/Examples/2D RPG Platformer/Scripts/GameManager.cs
+++ Assets/Examples/2D RPG Platformer/Scripts/GameManager.cs
@@ -25,10 +25,13 @@ namespace Platformer
             coinText.text = coinsCounter.ToString();
             if(player.deathState == true)
             {
+                
                 playerGameObject.SetActive(false);
+                //Change player to dead player prefab
                 GameObject deathPlayer = (GameObject)Instantiate(deathPlayerPrefab, playerGameObject.transform.position, playerGameObject.transform.rotation);
                 deathPlayer.transform.localScale = new Vector3(playerGameObject.transform.localScale.x, playerGameObject.transform.localScale.y, playerGameObject.transform.localScale.z);
                 player.deathState = false;
+                // Reload the scene after 3 seconds
                 Invoke("ReloadLevel", 3);
             }
         }
